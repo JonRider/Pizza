4ad86c5e6f8ff1424495c3c4b84e017ea8a8fe03
JonRider <jon@rider.net>
2019-09-01 15:11:17 +0530

    User registration

--- orders/urls.py
+++ orders/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("order", views.order, name="order"),
     path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout")
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -16,15 +17,33 @@ def index(request):
     }
     return render(request, "orders/index.html", context)
 
-def login_view(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+def register(request):
+    if request.method == "GET":
+        return render(request, "orders/register.html", {"message": None})
     else:
-        return render(request, "orders/login.html", {"message": "Invalid credentials."})
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            user = User.objects.create_user(username, email, password)
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "orders/register.html", {"message": "User already exists."})
+
+def login_view(request):
+    if request.method == "GET":
+        return render(request, "orders/login.html", {"message": None})
+    else:
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "orders/login.html", {"message": "Invalid credentials."})
 
 def logout_view(request):
     logout(request)
