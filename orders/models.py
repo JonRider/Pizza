53095d0884a996c5621c89663eae6c1f4c08f414
JonRider <jon@rider.net>
2019-09-03 16:00:04 +0530

    Add sicilians from menu

--- orders/models.py
+++ orders/models.py
@@ -34,11 +34,11 @@ class Regular(models.Model):
 
 class Sicilian(models.Model):
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE) # Number of topping
-    price_small = models.DecimalField(max_digits=5, decimal_places=2)
-    price_large = models.DecimalField(max_digits=5, decimal_places=2)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
-        return f"sicilian pizza with {self.choice} topping(s)"
+        return f"{self.size} sicilian pizza with {self.choice} topping(s)"
 
 
 class Topping(models.Model):
