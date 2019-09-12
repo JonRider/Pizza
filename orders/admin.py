27371bc8338ce08c8269626190720a5d78104d16
JonRider <jon@rider.net>
2019-09-12 16:32:23 +0530

    SicilianItem Admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Choice, Topping, Sicilian, Size, Regular, Cart, RegularItem
+from .models import Choice, Topping, Sicilian, Size, Regular, Cart, RegularItem, SicilianItem
 # Register your models here.
 
 class CartAdmin(admin.ModelAdmin):
@@ -8,10 +8,14 @@ class CartAdmin(admin.ModelAdmin):
 class RegularAdmin(admin.ModelAdmin):
     filter_horizontal = ("toppings",)
 
+class SicilianItemAdmin(admin.ModelAdmin):
+    filter_horizontal = ("toppings",)
+
 admin.site.register(Choice)
 admin.site.register(Size)
 admin.site.register(Regular)
 admin.site.register(Sicilian)
 admin.site.register(Topping)
 admin.site.register(RegularItem, RegularAdmin)
+admin.site.register(SicilianItem, SicilianItemAdmin)
 admin.site.register(Cart, CartAdmin)
